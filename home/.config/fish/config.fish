0fb9d8afd06ded5b7640f543f8ca4bded0ec211a
ortfero <ortfero@gmail.com>
2021-04-14 16:35:07 +0300

    zzz, reboot, halt without password

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -6,26 +6,56 @@ end
 
 
 function fish_prompt
+    set time (date +%H:%M)
+    read -l capacity < /sys/class/power_supply/BAT0/capacity
+    set dir (prompt_pwd)
     set_color green
-    echo -n ' > '
+    printf "[%s %d%% %s] > " $time $capacity $dir
 end
 
 
-alias ls='exa --icons'
-alias tree='exa --icons --tree'
-alias free='free -m'
+abbr -ag pi doas xbps-install -S
+abbr -ag pu doas xbps-install -Syu
+abbr -ag pr doas xbps-remove -o
+abbr -ag pf xbps-query -Rs
+abbr -ag pl xbps-query -Rf
+abbr -ag gst git status
+abbr -ag gadd git add
+abbr -ag grm git rm
+abbr -ag gcmt git commit -m
+abbr -ag gpsh git push
+abbr -ag gpll git pull
 
-abbr -a -g xi doas xbps-install -S
-abbr -a -g xu doas xbps-install -Syu
-abbr -a -g xr doas xbps-remove -o
-abbr -a -g xf xbps-query -Rs
-abbr -a -g xl xbps-query -Rf
-abbr -a -g gss git status
-abbr -a -g gadd git add
-abbr -a -g grm git rm
-abbr -a -g gct git commit -m
-abbr -a -g gpsh git push
-abbr -a -g gpll git pull
+
+function ls
+    exa
+end
+
+
+function tree
+    exa --tree
+end
+
+
+function free
+    free -m
+end
+
+
+function zzz
+    doas zzz
+end
+
+
+function reboot
+    doas reboot
+end
+
+
+function halt
+    doas halt
+end
+
 
 function battery
     if test ! -e /sys/class/power_supply/BAT0/status
@@ -40,4 +70,16 @@ end
 function mkcd
     mkdir -p $argv
     cd $argv
+end
+
+
+function brightness
+    if not set -q argv[1]
+        read -l brightness < /sys/class/backlight/amdgpu_bl0/brightness
+        set percents (math "round($brightness * 100 / 255)")
+        printf "brightness %d%%" $percents
+    else
+        set brightness (math "round($argv[1] * 255 / 100)")
+        echo $brightness > /sys/class/backlight/amdgpu_bl0/brightness
+    end
 end
